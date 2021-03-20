d2543c8d67cae1b5bdadeaa808d55e27198179fc
DrayItadakimasu <53301836+DrayItadakimasu@users.noreply.github.com>
2021-03-20 04:46:27 +0300

    ddd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+import Masonry from 'masonry-layout';
+const CONTAINER = 1030;
+const GRID = new Masonry('.info-box-grid',{
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-item',
+    percentPosition: true,
+    gutter: 30
+})
+
