15311d89be745ccb26fca89ae46cae5d498c4e0f
DrayItadakimasu <53301836+DrayItadakimasu@users.noreply.github.com>
2021-03-22 10:51:58 +0300

    ввв

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import Masonry from 'masonry-layout';
-const CONTAINER = 1030;
-const GRID = new Masonry('.info-box-grid',{
+import Axios from 'axios';
+const GRID = new Masonry('.info-box-grid', {
     itemSelector: '.grid-item',
     columnWidth: '.grid-item',
     percentPosition: true,
     gutter: 30
 })
+Callback()
+function Callback(form = '#callback-form') {
+    let _form = document.querySelector(form);
+    if (!_form) return;
+    const Validate = () => {
+        let valid = true;
+        if(_form.name.value.trim() === '') valid = false;
+        if (_form.contact.value.trim() === '') valid = false;
+        return valid;
+    }
+    const Send = (e) => {
+        e.preventDefault();
+        if(Validate()){
+            Axios.post('',new FormData(_form))
+                .then(({data})=>{
+                    Clear();
+                })
+        }else{
 
+            Clear();
+        }
+    }
+    const Clear = () => {
+        for (let item in _form){
+            _form[item].value = '';
+        }
+    }
+    _form.onsubmit = Send;
+}
